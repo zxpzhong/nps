e53df0cede6c2088a81893bc7fa0618d4afa1441
yisier <1340976576@qq.com>
2024-01-31 22:53:30 +0800

    版本号将继续沿用 0.26.xx

--- lib/version/version.go
+++ lib/version/version.go
@@ -1,8 +1,8 @@
 package version
 
-const VERSION = "0.27.01"
+const VERSION = "0.26.17"
 
 // Compulsory minimum version, Minimum downward compatibility to this version
 func GetVersion() string {
-	return "0.27.0"
+	return "0.26.0"
 }
